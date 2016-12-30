080683d659b189931bc3e512a019951f595a7164
Akinjide Bankole <r@akinjide.me>
2016-12-30 03:26:30 +0100

    fix(contact): update origin to all addr

--- contact.php
+++ contact.php
@@ -11,7 +11,7 @@
   $data = array();
 
   header('Content-Type: application/json');
-  header('Access-Control-Allow-Origin: http://localhost:8000');
+  header('Access-Control-Allow-Origin: *');
   header('Access-Control-Allow-Credentials: true');
 
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
